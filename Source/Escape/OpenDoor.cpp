6a9f0dba6f6378338164d1d4722b62cecf1e0a2c
NickJ <vmelodyj@gmail.com>
2020-07-23 23:01:53 -0700

    Added door open/close function

---
+++ Source/Escape/OpenDoor.cpp
@@ -0,0 +1,93 @@
+// CopyRight Nick 2020
+
+
+#include "OpenDoor.h"
+#include "GameFramework/PlayerController.h"
+#include "Engine/World.h"
+#include "GameFramework/Actor.h"
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	InitialYaw = GetOwner()->GetActorRotation().Yaw;
+	CurrentYaw = InitialYaw;
+	TargetYaw += InitialYaw;
+
+	if (!PressurePlate) {
+		UE_LOG(LogTemp, Error, TEXT("%s has the open door component on it, but no pressureplate set"), *GetOwner()->GetName());
+	}
+
+
+	ActorThatOpen = GetWorld()->GetFirstPlayerController()->GetPawn();
+
+	// FRotator CurrentRotation =  GetOwner()->GetActorRotation();
+	// FRotator OpenDoorRotation {0.f, (CurrentRotation.Yaw + 90.f), 0.f};
+
+
+	// GetOwner()->SetActorRotation(OpenDoorRotation);
+
+	/* FRotator(
+		y,
+		z,
+		x
+	)
+
+	*/
+	// ...
+	
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+	// UE_LOG(LogTemp, Warning, TEXT("%s"), *GetOwner()->GetActorRotation().ToString());
+	// UE_LOG(LogTemp, Warning, TEXT("Yaw is: %f"), GetOwner()->GetActorRotation().Yaw);
+
+	if (PressurePlate && PressurePlate->IsOverlappingActor(ActorThatOpen)){
+
+		OpenDoor(DeltaTime);
+		DoorLastOpened = GetWorld()->GetTimeSeconds();
+	}
+
+	if (GetWorld()->GetTimeSeconds() - DoorLastOpened > DoorCloseDelay){
+
+		CloseDoor(DeltaTime);
+	}
+
+	// float CurrentYaw =  GetOwner()->GetActorRotation().Yaw;
+	// FRotator OpenDoorRotation {0.f, (CurrentYaw + 90.f), 0.f};
+	// OpenDoorRotation.Yaw = FMath::FInterpTo(CurrentYaw, CurrentYaw + 90.f, DeltaTime, 2);
+	// GetOwner()->SetActorRotation(OpenDoorRotation);
+// 
+}
+void UOpenDoor::OpenDoor(float DeltaTime){
+	CurrentYaw = FMath::Lerp(CurrentYaw, TargetYaw, DeltaTime * 0.8f);
+	FRotator DoorRotation = GetOwner()->GetActorRotation();
+	DoorRotation.Yaw = CurrentYaw;
+	GetOwner()->SetActorRotation(DoorRotation);
+
+}
+
+void UOpenDoor::CloseDoor(float DeltaTime){
+	CurrentYaw = FMath::Lerp(CurrentYaw, InitialYaw, DeltaTime * 2.f);
+	FRotator DoorRotation = GetOwner()->GetActorRotation();
+	DoorRotation.Yaw = CurrentYaw;
+	GetOwner()->SetActorRotation(DoorRotation);
+
+}
---
+++ Source/Escape/OpenDoor.h
@@ -0,0 +1,51 @@
+// CopyRight Nick 2020
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+
+#include "Engine/TriggerVolume.h"
+
+#include "OpenDoor.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class ESCAPE_API UOpenDoor : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UOpenDoor();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	void OpenDoor(float DeltaTime);
+	void CloseDoor(float DeltaTime);
+
+private:
+
+float InitialYaw;
+float CurrentYaw;
+
+UPROPERTY(EditAnywhere)
+float TargetYaw{90.f};
+
+UPROPERTY(EditAnywhere)
+ATriggerVolume *PressurePlate;
+
+
+UPROPERTY(EditAnywhere)
+AActor *ActorThatOpen;
+
+		
+float DoorLastOpened {0.f};
+float DoorCloseDelay = 0.5f;
+
+};
